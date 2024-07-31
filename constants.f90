b7ae955131d6d4ab2197eb0ae7ef1dd19f86bf3c
YEZZFUSL <18398621+yezzfusl@users.noreply.github.com>
2024-07-31 09:11:15 -0400

    Define physical and mathematical constants.

---
+++ constants.f90
@@ -0,0 +1,6 @@
+module constants
+  implicit none
+  real, parameter :: PI = 3.14159265358979323846
+  real, parameter :: G = 9.81 ! Acceleration due to gravity (m/s^2)
+  real, parameter :: R_GAS = 287.058 ! Specific gas constant for air (J/(kg·K))
+end module constants
